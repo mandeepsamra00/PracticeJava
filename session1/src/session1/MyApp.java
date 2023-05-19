bf13ed95501c2929167488a9daf8b5e6e309d54f
mandeepsamra <mksaram00@gmail.com>
2023-05-19 14:13:20 +0000

    session1 java

---
+++ session1/src/session1/MyApp.java
@@ -0,0 +1,11 @@
+package session1;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("welcome to java session");
+	System.out.println("Be Exceptional!!");
+	}
+
+}
